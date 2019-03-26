41787d53c597ed079a54b9a4a10b02402603bce5
Lucian Petre Stere <lucian.stere@outlook.com>
2019-03-26 15:51:56 +0200

    hooks app si refactor la seasons

---
+++ hooks-simple/src/components/UserList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import useResouces from './useResource';
+
+
+const UserList = () => {
+  const users = useResouces('users');
+
+  return (
+      <ul>
+        {users.map(user => (
+            <li key={user.id}>
+                {user.name}
+            </li>))}
+      </ul>
+  );
+};
+
+export default UserList;
--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,38 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import SeasonDisplay from './SeasonDisplay';
 import Loader from './Loader';
+import useLocation from './useLocation';
 
-class App extends React.Component  {
-   state = { lat: null, errorMessage: '' };
+const App = () => {
+  const [ lat, errorMessage ] = useLocation();
 
-  componentDidMount() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => this.setState({ lat: position.coords.latitude }),
-      err => this.setState({ errorMessage: err.message }),
-    );
+  let content;
+  if(errorMessage) {
+    content = <div>Error: {errorMessage}</div>;
+  } else if(lat) {
+    content = <SeasonDisplay lat={ lat } />;
+  } else {
+    content = <Loader message="Please accept location request!" />;
   }
 
-  componentDidUpdate() {
-    // console.log('My component was just updated - it rerendered');
-  }
-
-  renderContent() {
-    if(this.state.errorMessage && !this.state.lat) {
-      return <div>Error: {this.state.errorMessage}</div>
-    }
-
-    if(!this.state.errorMessage && this.state.lat) {
-      return <SeasonDisplay lat={ this.state.lat }/>
-    }
-
-    return <div><Loader message="Please accept location request!"/></div>
-  }
-
-  // React says we have to define render!
-  render() {
-    return <div className="border red">{this.renderContent()}</div>;
-  }
+  return <div className="border read">{content}</div>
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'))
